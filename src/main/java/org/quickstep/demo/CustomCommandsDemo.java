fff5d03e83f9ea95360ca52e31070122984cf387
outerloper <Konrad.Sacala@gmail.com>
2014-08-22 11:54:17 +0200

    - adding SplitPane builder to CustomCommandDemo

--- src/main/java/org/quickstep/demo/CustomCommandsDemo.java
+++ src/main/java/org/quickstep/demo/CustomCommandsDemo.java
@@ -1,33 +1,55 @@
 package org.quickstep.demo;
 
+import java.awt.*;
 import java.util.*;
 import java.util.List;
 import javax.swing.*;
 
 import org.quickstep.*;
 
+import static javax.swing.UIManager.*;
 import static org.quickstep.GridBagToolKit.*;
 
 public class CustomCommandsDemo extends JFrame
 {
    public CustomCommandsDemo()
    {
+      JPanel explicitlyProvidedPanel = new JPanel();
+      explicitlyProvidedPanel.setBackground(Color.GREEN);
+
       buildContent(this, panel().
          withContentAnchor(A.BOTH).
          withOrientation(Orientation.VERTICAL).
          add(toolbar().
-            add(new JButton(UIManager.getIcon("FileChooser.newFolderIcon"))).
-            add(new JButton(UIManager.getIcon("FileChooser.upFolderIcon"))).
+            add(new JButton(getIcon("FileChooser.newFolderIcon"))).
+            add(new JButton(getIcon("FileChooser.upFolderIcon"))).
             addSeparator().
-            add(new JButton(UIManager.getIcon("FileChooser.detailsViewIcon"))).
-            add(new JButton("Details..."))).
+            add(new JButton(getIcon("FileChooser.detailsViewIcon"))).
+            add(new JButton("Details..."))
+         ).
          add(tabs().
             add(tab("First").add("First tab content")).
-            add(tab("Second").add("Second tab content")).
-            add(tab("Third").add("Third tab content")).
-            add(tab("Fourth").add("Fourth tab content")).
+            add(tab("Second").
+               withContentAnchor(A.BOTH).
+               add(split(Orientation.VERTICAL).
+                  withResizeWeight(0.67).
+                  withFirst(panel().add("Third tab top content")).
+                  withSecond(panel().add("Third tab bottom content"))
+               )
+            ).
+            add(tab("Third").
+               withContentAnchor(A.BOTH).
+               add(split(Orientation.HORIZONTAL).
+                  withFirst(panel().add("Third tab left content")).
+                  withSecond(panel().add("Third tab right content"))
+               )
+            ).
+            add(tab("Fourth").
+               with(explicitlyProvidedPanel).
+               add("Fourth tab content")
+            ).
             add(tab("Details").
-               withIcon(UIManager.getIcon("FileChooser.detailsViewIcon")).
+               withIcon(getIcon("FileChooser.detailsViewIcon")).
                add("<html><i>Details</i> tab content</html>")
             )
          ).
@@ -67,15 +89,19 @@ public class CustomCommandsDemo extends JFrame
       return new ButtonsCommand();
    }
 
+   public static SplitCommand split(Orientation orientation)
+   {
+      return new SplitCommand(orientation);
+   }
+
    public static void main(String[] args) throws Exception
    {
-      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+      setLookAndFeel(getSystemLookAndFeelClassName());
       new CustomCommandsDemo();
    }
 }
 
-// TODO rename CellCommand to AbstractComponentBuilder
-// TODO parameterize ContainerCommand also with target component type?
+// TODO think about renaming CellCommand to AbstractComponentBuilder, then GridBagBuilder -> GridBuilderSupport?
 class ToolBarCommand extends GridContainerCommand<ToolBarCommand>
 {
    @Override
@@ -104,7 +130,7 @@ class TabsCommand extends CellCommand<TabsCommand>
       int i = 0;
       for (TabCommand tab : tabs)
       {
-         result.add(tab.getTitle(), tab.getComponent());
+         result.add(tab.getTitle(), panel().withContentAnchor(A.BOTH).add(tab).getComponent());
          if (tab.getIcon() != null)
          {
             result.setIconAt(i, tab.getIcon());
@@ -146,6 +172,13 @@ class TabCommand extends GridContainerCommand<TabCommand>
       this.icon = icon;
       return this;
    }
+
+   // TODO fins sth more elegant with this default spec..
+   @Override
+   protected CellSpec getSpec(Orientation parentOrientation)
+   {
+      return spec().withInset(5).overrideWith(super.getSpec(parentOrientation));
+   }
 }
 
 class ButtonsCommand extends CellCommand<ButtonsCommand>
@@ -158,7 +191,6 @@ class ButtonsCommand extends CellCommand<ButtonsCommand>
       return this;
    }
 
-   // TODO rename parameters to parentOrientation and parentFactory
    @Override
    public JComponent getComponent(Orientation parentOrientation, ComponentFactory parentFactory)
    {
@@ -178,5 +210,53 @@ class ButtonsCommand extends CellCommand<ButtonsCommand>
    protected CellSpec getSpec(Orientation parentOrientation)
    {
       return spec().overrideWith(super.getSpec(parentOrientation).withWeightY(0.0));
+   }
+}
+
+class SplitCommand extends CellCommand<SplitCommand>
+{
+   private PanelCommand first;
+   private PanelCommand second;
+
+   private Orientation orientation = Orientation.HORIZONTAL;
+   private double resizeWeight = 0.5;
+
+   SplitCommand(Orientation orientation)
+   {
+      this.orientation = orientation;
+   }
+
+   @Override
+   public JComponent getComponent(Orientation parentOrientation, ComponentFactory parentFactory)
+   {
+      JSplitPane result = new JSplitPane(orientation.isHorizontal() ? JSplitPane.HORIZONTAL_SPLIT : JSplitPane.VERTICAL_SPLIT);
+      if (first != null)
+      {
+         result.setTopComponent(first.getComponent());
+      }
+      if (second != null)
+      {
+         result.setBottomComponent(second.getComponent());
+      }
+      result.setResizeWeight(resizeWeight);
+      return result;
+   }
+
+   public SplitCommand withResizeWeight(double resizeWeight)
+   {
+      this.resizeWeight = resizeWeight;
+      return this;
+   }
+
+   public SplitCommand withFirst(PanelCommand first)
+   {
+      this.first = first;
+      return this;
+   }
+
+   public SplitCommand withSecond(PanelCommand second)
+   {
+      this.second = second;
+      return this;
    }
 }
