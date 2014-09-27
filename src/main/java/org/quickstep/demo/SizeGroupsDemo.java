4d8cb765ec50e96641c8f5945202c344e6301bea
Konrad <Konrad.Sacala@gmail.com>
2014-09-27 17:25:51 +0200

    - demo for size groups

---
+++ src/main/java/org/quickstep/demo/SizeGroupsDemo.java
@@ -0,0 +1,37 @@
+package org.quickstep.demo;
+
+import javax.swing.*;
+
+import static org.quickstep.GridBagToolKit.*;
+
+public class SizeGroupsDemo
+{
+   public static void main(String[] args)
+   {
+      DemoUtils.setSystemLookAndFeel();
+      JFrame frame = new JFrame();
+
+      buildContent(
+         frame, panel()
+            .withContentAnchor(A.BOTH)
+            .add(new JButton("0"), spec().withSizeGroup(0))
+            .add(new JButton("00"), spec().withSizeGroup(0))
+            .add(new JButton("000"), spec().withSizeGroup(0))
+            .addSeparator()
+            .add(new JButton("111"), spec().withSizeGroup(1))
+            .add(new JButton("11111"), spec().withSizeGroup(1))
+            .add(new JButton("1111111"), spec().withSizeGroup(1))
+            .addSeparator()
+            .add(new JButton("22222"), spec().withSizeGroup(2))
+            .add(new JButton("222222222"), spec().withSizeGroup(2))
+            .add(new JButton("2222222222222"), spec().withSizeGroup(2))
+      );
+
+      frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+      frame.setMinimumSize(frame.getPreferredSize());
+      frame.setLocationRelativeTo(null);
+      frame.setAlwaysOnTop(true);
+      frame.setVisible(true);
+      frame.pack();
+   }
+}
